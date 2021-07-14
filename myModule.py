a786db3607731d2ab701530203c09d0a18f3b914
usakocher <adammkocher@gmail.com>
2021-07-14 17:07:25 -0400

    submit homework

---
+++ myModule.py
@@ -0,0 +1,7 @@
+from math import pi
+
+def sqFoot(a, b):
+   print(f'The square footage of a house of length {a} and width {b} is {a*b} sq feet')
+
+def circumference(r):
+   print(f'The circumference of a circle of radius {r} is {2 * pi * r}')
