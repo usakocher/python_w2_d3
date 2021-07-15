f8a63f15d42d0bd07f2f356b645021fc5dca55bc
usakocher <adammkocher@gmail.com>
2021-07-15 00:11:42 -0400

    Final tweaks

--- myModule.py
+++ myModule.py
@@ -1,7 +1,10 @@
 from math import pi
 
-def sqFoot(a, b):
-   print(f'The square footage of a house of length {a} and width {b} is {a*b} sq feet')
+def sqFoot():
+    a = int(input('What is the length of your house? '))
+    b = int(input('What is the width of your house? '))
+    return f'The square footage of a house of length {a} and width {b} is {a*b} sq feet'
 
-def circumference(r):
-   print(f'The circumference of a circle of radius {r} is {2 * pi * r}')
+def circumference():
+    r = input('What is the radius of the circle? ')
+    return f'The circumference of a circle of radius {r} is {round(2 * pi * float(r), 2)}'
